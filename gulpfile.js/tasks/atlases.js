c067751b8a7513e0521ed0e9c9d532b4bcda7068
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-27 12:52:01 -0300

    One task for each Atlas Texture
    Instead of generating a single task for all texture atlases that want to be
    processed, create individual tasks, each dealing with its corresponding
    directory.
    
    Tasks are named using the pattern `atlas:<directory>`. Available tasks can be
    listed using `gulp --tasks-simple`.

--- gulpfile.js/tasks/atlases.js
+++ gulpfile.js/tasks/atlases.js
@@ -3,20 +3,16 @@
 
 module.exports = function (gulp, $, config) {
 
-  var fs    = require('fs');
-  var path  = require('path');
-  var merge = require('merge-stream');
+  var fs   = require('fs');
+  var path = require('path');
 
   var dirs = config.dirs;
 
   function pot (n) {
     var m = 2;
-    while (m < n) m *= 2;
+    while (m < n) { m *= 2 }
     return m;
   }
-
-  // Modified version of this recipe:
-  // https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-task-steps-per-folder.md
 
   function getFolders (dir) {
     return fs.readdirSync(dir)
@@ -25,40 +21,53 @@ module.exports = function (gulp, $, config) {
       });
   }
 
-  gulp.task('atlas', function () {
-    var folders = getFolders(dirs['raw-assets']);
+  function generateAtlas (name) {
+    return $.spritesmith({
+      cssTemplate : 'src/atlas.template',
+      cssName     : name + '.js',
+      imgName     : name + '.png',
+      padding     : 8
+    });
+  }
 
-    var tasks = folders.map(function (name) {
+  function resizeImage () {
+    return $.gm(function (gmFile, done) {
+      gmFile.size(function (err, size) {
+        var w = pot(size.width);
+        var h = pot(size.height);
+
+        done(null, gmFile
+          .set('colorspace', 'RGB')
+          .background('none')
+          .gravity('northwest')
+          .extent(w, h));
+      });
+    }, { imageMagick: true });
+  }
+
+  function createAtlasTask (name) {
+    return function () {
       var folder = path.join(dirs['raw-assets'], name, '*.png');
       var spriteData = gulp.src(folder, { read: false })
-        .pipe($.spritesmith({
-          cssTemplate : 'src/atlas.template',
-          cssName     : name + '.js',
-          imgName     : name + '.png',
-          padding     : 8
-        }));
+        .pipe(generateAtlas(name));
 
+      // Resize generated texture dimensions to a power of 2.
       spriteData.img
-        .pipe($.gm(function (gmFile, done) {
-          gmFile.size(function (err, size) {
-            var w = pot(size.width);
-            var h = pot(size.height);
-
-            done(null, gmFile
-              .set('colorspace', 'RGB')
-              .background('none')
-              .gravity('northwest')
-              .extent(w, h));
-          });
-        }, { imageMagick: true }))
+        .pipe(resizeImage())
         .pipe(gulp.dest(dirs['assets']));
 
+      // Place the generated module in the source base.
       spriteData.css.pipe(gulp.dest(dirs['atlases']));
 
       return spriteData;
-    });
+    };
+  }
 
-    return merge(tasks);
-  });
+  getFolders(dirs['raw-assets'])
+    .forEach(function (folder) {
+      var taskName = 'atlas:' + folder;
+      gulp.task(taskName, createAtlasTask(folder));
+      return taskName;
+    });
 
 };
